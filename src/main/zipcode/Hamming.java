e32f7a57c953a1689bad1e12af067337a1960559
nora <sxj042@gmail.com>
2023-04-10 18:30:23 -0400

    finished

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,23 +1,21 @@
 package zipcode;
 
 public class Hamming {
-    String s1;
-    String s2;
-    public Hamming(String s1, String s2) {
+    private String s1;
+    private String s2;
+    public Hamming(String s1, String s2) throws IllegalArgumentException{
         this.s1 = s1;
         this.s2 = s2;
+        if (s1.length() != s2.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-
         char[] a = s1.toCharArray();
         char[] b = s2.toCharArray();
         int count = 0;
-//
-//        try {
-            if (a.length != b.length) {
-                throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-            }
+
 
             for (int i = 0; i < s1.length(); i++) {
                 if (a[i] != b[i]) {
@@ -25,11 +23,6 @@ public class Hamming {
                 }
             }
             return count;
-//        }
-//        catch (IllegalArgumentException e) {
-//            System.out.println("leftStrand and rightStrand must be of equal length.");
-//
-//        }
-//        return -1;
+
     }
 }
